3b6a4530390ce2baa3105d6550409cf70532b705
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2020-05-29 22:12:35 -0300

    Melhoria no bootstrap.

--- topicos-fatec-2020/src/main/java/br/edu/fatec2020/topicos/domain/CarrinhoCompra.java
+++ topicos-fatec-2020/src/main/java/br/edu/fatec2020/topicos/domain/CarrinhoCompra.java
@@ -3,10 +3,8 @@ package br.edu.fatec2020.topicos.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.annotation.SessionScope;
 
 import lombok.Getter;
 
@@ -14,8 +12,11 @@ import lombok.Getter;
 //requisições do mesmo navegador. Se fecharmos a janela toda
 //do navegador e todas as abas do site aberto, ele perde essa
 //referência da session. Se tentar abrir em outro navegador,
-//ou janela anônima, será outra session. (scopeName = WebApplicationContext.SCOPE_SESSION)
-@Scope(scopeName = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS )
+//ou janela anônima, será outra session.
+/*é possível substituir aquela linha com @Scope(...) apenas por @SessionScope (sem nenhum parâmetro), 
+ *ficando muito mais curta a definição*/
+//@Scope(scopeName = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS )
+@SessionScope()
 @Getter
 @Component
 public class CarrinhoCompra 
