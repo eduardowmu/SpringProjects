31c1c1b96bf41cf6660892d6b3f4a2bf991cb331
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2021-02-09 23:57:32 -0300

    Criptografando senha

--- demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
+++ demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.mballem.curso.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.mballem.curso.security.service.UserService;
 
 @EnableWebSecurity
 public class SecurityConfig extends 
 	WebSecurityConfigurerAdapter
-{	@Override protected void configure(HttpSecurity http) 
+{	//injeção de dependencias
+	@Autowired private UserService usuarioService;
+	
+	@Override protected void configure(HttpSecurity http) 
 		throws Exception
 	{	/*Essa é a configuração mais básica que temos para
 	 	trabalhar com Spring Security*/
@@ -44,5 +52,26 @@ public class SecurityConfig extends
 				.logoutSuccessUrl("/");
 			
 		//super.configure(http);
-	}	
+	}
+
+	/*Método que será sobreescrito de WebSecurityConfigureAdapter*/
+	@Override protected void configure(
+			AuthenticationManagerBuilder auth) throws Exception 
+	{	/*Este método espera que passemos como parâmetro um 
+		objeto do tipo userDetailsService, que é nosso service,
+		pois esta implementa um UserDetailService.*/
+		auth.userDetailsService(this.usuarioService)
+			/*O objetivo desse método é informar qual o tipo de
+			 *criptografia que será utilizado. Quando formos fazer
+			 *Um cadastro de usuário iremos pegar a senha digitado
+			 *no cadastro e vai criptografar e salvar a senha
+			 *criptografada. Quando o spring receber os dados da
+			 *consulta que foi passado para ele e for comparar com
+			 *a senha digitada, será necessário criptografar a senha
+			 *para ver se essa criptografia é igual aquela que
+			 *temos no BD*/
+			.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	
 }
