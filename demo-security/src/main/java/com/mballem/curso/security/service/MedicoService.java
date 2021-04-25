13a93e952e10270519ec17c249274849ad84024f
eduardo.murakoshi@groove.tech <eduardo.murakoshi@groove.tech>
2021-04-25 00:05:34 -0300

    salvar e editar medico

--- demo-security/src/main/java/com/mballem/curso/security/service/MedicoService.java
+++ demo-security/src/main/java/com/mballem/curso/security/service/MedicoService.java
@@ -16,7 +16,32 @@ public class MedicoService
 	@Transactional(readOnly = true)
 	public Medico findUserById(Long id) 
 	{	return this.repository.findByUsuarioId(id)
-			/*caso não encontre o medico desejado, evita o NullPointer*/
+			/*caso não encontre o medico desejado, 
+			 *evita o NullPointer*/
 			.orElse(new Medico());
+	}
+	
+	@Transactional(readOnly = false)
+	public void salvar(Medico medico) 
+	{this.repository.save(medico);}
+	
+	/*O interessante é que não precisamos fazer o método
+	 *save, pois como criamos um objeto persistente, o hibernate
+	 *já faz isso automaticamente.*/
+	@Transactional(readOnly = false)
+	public void editar(Medico medico) 
+	{	//Criando um médico persistente
+		Medico medicoEditar = this.repository.findById(
+			medico.getId()).get();
+		medicoEditar.setCrm(medico.getCrm());
+		medicoEditar.setDtInscricao(medico.getDtInscricao());
+		medicoEditar.setNome(medico.getNome());
+		/*Teste que verifica se a lista de especialidades não
+		 *está vazia, ou seja, se o usuário digitou a especialidade
+		 *devemos inserir no banco de dados*/
+		if(!medico.getEspecialidades().isEmpty())
+		{	medicoEditar.getEspecialidades().addAll(
+				medico.getEspecialidades());
+		}
 	}
 }
